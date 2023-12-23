b5eaa86d91c6822fe1fc4c302266060499853578
Kenechi <nsudekenechi2@gmail.com>
2023-12-23 09:04:41 +0100

    Finished Get Category Controller

--- backend/controllers/categories.js
+++ backend/controllers/categories.js
@@ -1,12 +1,28 @@
 const model = require("../models/categories");
+// @desc GET /api/posts/categories 
+const getCategories = async (req, res) => {
+    try {
+        const categories = await model.find();
+        res.status(201).json(categories)
+    } catch (err) {
+        res.status(400).json(err.message)
+    }
+}
 // @desc CREATE /api/posts/categories  
 const addCategory = async (req, res) => {
     try {
         const { name } = req.body;
-        if(!name) throw new Error("Fields cannot be empty")
+        if (!name) throw new Error("Fields cannot be empty")
         const category = await model.create({ name })
         res.status(201).json(category)
     } catch (err) {
         res.status(400).json(err.message)
     }
+}
+
+// @desc patch /api/posts/categories /:id
+
+
+module.exports = {
+    addCategory
 }
