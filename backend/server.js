20cf35c9c05765b23ca32cc69f54309261ac4ca6
Kenechi <nsudekenechi2@gmail.com>
2023-12-25 02:15:31 +0100

    Testing api

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,16 @@ const DB_Connect = require("./db/db");
 const auth = require("./middlewares/auth");
 const port = process.env.PORT || 5001
 // Connecting to database
-// DB_Connect();
+DB_Connect();
 // Telling the type of data body should accept
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 // Specifying the routes for users
-app.use("/", (req, res) => {
-    res.status(201).json("Testing")
-})
-// app.use("/api/users", require("./routers/users"))
+app.use("/api/users", require("./routers/users"))
 // Specifying routes for blog posts
-// app.use("/api/posts", auth, require("./routers/posts"))
+app.use("/api/posts", auth, require("./routers/posts"))
 // Specifying routes for blog categories
-// app.use("/api/posts/categories", auth, require("./routers/categories"))
+app.use("/api/posts/categories", auth, require("./routers/categories"))
 // Connecting to server
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
